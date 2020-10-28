b7566ca3dff07060c3abb458a8025584437170ce
Anurag30891 <51212490+Anurag30891@users.noreply.github.com>
2020-10-28 17:49:23 +0530

    method added by developer 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,10 @@ public class DemoApplication {
 			System.out.println("Welcome");
 		}
 	}
+	
+	public void addedMethod(){
+		System.out.println("new method")
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
