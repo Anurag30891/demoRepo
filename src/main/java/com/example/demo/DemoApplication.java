a6375b4e80fb117a0818112bc0544aa94664a82b
Anurag <anuragsingh.flosteq@gmail.com>
2020-10-28 16:51:56 +0530

    added if condition to check the data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	public void addData(String data) {
+		System.out.println("data =" + data);
+		if (data.equals("anurag")) {
+			System.out.println("Welcome");
+		}
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
